9ea5017ada234caa556f20a2b1d53f4e89102bc1
arianylopez <159574397+arianylopez@users.noreply.github.com>
2024-03-16 00:22:53 -0400

    prueba 2

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -34,7 +34,7 @@ void exercise_2(double A, double B, double C){
   } else if (A==0){
     cout << -C/B << endl;
   } else{
-    cout <<" ";
+    cout <<"";
   }
 }
 
@@ -105,17 +105,7 @@ void exercise_12(string color1, int numb1,
 
 string exercise_13(int age, int years_of_experience) {
   // TODO: YOUR CODE HERE
-  cin>>age;
-  cin>>years_of_experience;
-  if(age>=18 && years_of_experience>=3){
-    return "Project manager";
-  }else if(age>=18 && years_of_experience>=5){
-    return "Senior project manager";
-  }else if(age>=18 && years_of_experience==0){
-    return "Project coordinator";
-  } else{
-    return "Not eligible";
-  }
+  if(age>=18 && years_of_experience
 }
 
 string exercise_14(int number_of_docs) {
